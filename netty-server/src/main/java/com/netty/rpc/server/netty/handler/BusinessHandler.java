399b8277ab8c423cdf53ce30f20087a347cd42c6
vincentAffinity <yanyibin@jmgo.com>
2021-09-25 10:40:19 +0800

    SimpleChannelInboundHandler在接收到数据后会自动release掉数据占用的Bytebuffer资源(自动调用Bytebuffer.release())。服务端不可使用。

--- netty-server/src/main/java/com/netty/rpc/server/netty/handler/BusinessHandler.java
+++ netty-server/src/main/java/com/netty/rpc/server/netty/handler/BusinessHandler.java
@@ -3,7 +3,7 @@ package com.netty.rpc.server.netty.handler;
 import com.netty.rpc.codec.RpcRequest;
 import com.netty.rpc.server.task.BusinessTask;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,7 +13,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 /**
  * netty server business logic process handler
  */
-public class BusinessHandler extends SimpleChannelInboundHandler<RpcRequest> {
+public class BusinessHandler extends ChannelInboundHandlerAdapter {
 
     private static final Logger logger = LoggerFactory.getLogger(BusinessHandler.class);
 
@@ -32,11 +32,11 @@ public class BusinessHandler extends SimpleChannelInboundHandler<RpcRequest> {
     /**
      * 获取rpcRequest进行业务处理
      * @param ctx
-     * @param request
+     * @param msg 信息
      */
     @Override
-    public void channelRead0(final ChannelHandlerContext ctx, final RpcRequest request) {
-        businessTaskThreadPool.execute(new BusinessTask(request, serviceKey2BeanMap, ctx));
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        businessTaskThreadPool.execute(new BusinessTask((RpcRequest) msg, serviceKey2BeanMap, ctx));
     }
 
     @Override
