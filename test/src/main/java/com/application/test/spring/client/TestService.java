11eaedbf3637875ee467ebf7a18c98db859728d5
vincentAffinity <yanyibin@jmgo.com>
2021-09-26 16:30:09 +0800

    非junit client spring启动

--- test/src/main/java/com/application/test/spring/client/TestService.java
+++ test/src/main/java/com/application/test/spring/client/TestService.java
@@ -2,14 +2,25 @@ package com.application.test.spring.client;
 
 import com.application.test.service.HelloService;
 import com.netty.rpc.annotation.BRpcConsumer;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestService {
 
     @BRpcConsumer(version = "1.0")
-    HelloService helloService;
+    static HelloService helloService;
 
     public void hello() {
         String cyx = helloService.hello("cyx");
         System.out.println("cyx ============================>>> " + cyx);
+    }
+
+    /**
+     * api
+     * @param args
+     */
+    public static void main(String[] args) {
+        new ClassPathXmlApplicationContext("client-spring.xml");
+        String name = helloService.hello("yyb");
+        System.out.println("name = " + name);
     }
 }
